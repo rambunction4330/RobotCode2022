a1dd41a2bff469fdf0d2dbd178d4efad626f9cfe
gcjurgiel <gcjurgiel@icloud.com>
2022-01-15 17:53:00 -0600

    Implemented mecanum-drive contructor

--- lib/src/drive/MecanumDrive.cpp
+++ lib/src/drive/MecanumDrive.cpp
@@ -1,6 +1,16 @@
 #include "rmb/drive/MecanumDrive.h"
 
 namespace rmb {
+  MecanumDrive::MecanumDrive(VelocityController<units::meters>& fl,
+                             VelocityController<units::meters>& fr,
+                             VelocityController<units::meters>& rl,
+                             VelocityController<units::meters>& rr,
+                             frc::MecanumDriveKinematics k,
+                             units::meters_per_second_t maxVel,
+                             units::meters_per_second_t maxRotVel) :
+                             frontLeft(fl), frontRight(fr), rearLeft(rl), rearRight(rr),
+                             kinematics(k), maxVelocity(maxVel), maxRotVelocity(maxRotVel) {}
+
   void MecanumDrive::driveWheelSpeeds(frc::MecanumDriveWheelSpeeds wheelSpeeds) {
     // Set velocity for each individual wheel
     frontLeft.setVelocity(wheelSpeeds.frontLeft);
