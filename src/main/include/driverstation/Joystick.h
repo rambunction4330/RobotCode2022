858b3359d79089acd099a1434ccff747c5c485b7
Sam <swiseman2005@gmail.com>
2022-01-18 17:50:52 -0600

    v1 Joystick class

--- src/main/include/driverstation/Joystick.h
+++ src/main/include/driverstation/Joystick.h
@@ -1,21 +1,56 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
+//@notSam25
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <frc/Joystick.h>
+#include <frc2/command/button/JoystickButton.h>
+#include <cmath>
+enum JoystickKey  {
+  TRIGGER = 1,
+  SIDETRIGGER,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  ELLEVEN,
+  TWELVE
+};
 
 class Joystick : public frc2::SubsystemBase {
  public:
-  Joystick();
 
-  /**
-   * Will be called periodically whenever the CommandScheduler runs.
-   */
-  void Periodic() override;
+  Joystick(int port = 0, float dz = 0.2f) : joystick(port), deadZone(dz) {} //Init constructer
 
- private:
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
+  double getX() {
+    return std::abs(joystick.GetX()) <= deadZone ? 0.0f : joystick.GetX(); //Get the Y val of the joystick, doesn't include values within deadzone
+  }  
+
+  double getY() {
+    return std::abs(joystick.GetY()) <= deadZone ? 0.0f : joystick.GetY(); //Get the Y val of the joystick, doesn't include values within deadzone
+  }  
+
+  inline double getXRaw() { //Gets Joystick Raw values witout deadzone
+    return joystick.GetX();
+  }
+
+  inline double getYRaw() { //Gets Joystick Raw values witout deadzone
+    return joystick.GetY();
+  }
+
+  frc2::JoystickButton getButton(JoystickKey button)  { //Gets the button passed through the enum
+    return frc2::JoystickButton(&joystick, (int)button);
+  }
+  
+  double getTwist() { //Gets joystick twist/rotation
+    double twist = (std::abs(std::pow(joystick.GetTwist(),2)) <= deadZone) ? 0.0 : std::pow(joystick.GetTwist(),2);
+    return (joystick.GetTwist() >= 0) ? twist : -twist;
+  }
+
+  private:
+    frc::Joystick joystick;
+    float deadZone = 0.2f;
 };
