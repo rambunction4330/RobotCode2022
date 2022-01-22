25ac6a9070462e5b858accec188e17397a9b998e
Abheek Dhawan <abheekd@protonmail.com>
2022-01-22 11:35:11 -0600

    Auto-format drive source files

--- lib/src/drive/HolonomicTrajectoryCommand.cpp
+++ lib/src/drive/HolonomicTrajectoryCommand.cpp
@@ -3,30 +3,28 @@
 #include "rmb/drive/HolonomicTrajectoryCommand.h"
 
 namespace rmb {
-    HolonomicTrajectoryCommand::HolonomicTrajectoryCommand(const frc::Trajectory& t,
-                                                           HolonomicDrive& d,
-                                                           const HolonomicDriveOdometry& o,
-                                                           frc::HolonomicDriveController& dc) :
-                                                           trajectory(t), drive(d), 
-                                                           odometry(o), driveController(dc) {}
-    
-    void HolonomicTrajectoryCommand::Initialize() {
-        timer.Reset();
-        timer.Start();
-    }
+HolonomicTrajectoryCommand::HolonomicTrajectoryCommand(
+    const frc::Trajectory &t, HolonomicDrive &d,
+    const HolonomicDriveOdometry &o, frc::HolonomicDriveController &dc)
+    : trajectory(t), drive(d), odometry(o), driveController(dc) {}
 
-    void HolonomicTrajectoryCommand::Execute() {
-        units::time::second_t currentTime = units::second_t(timer.Get());
-        frc::Trajectory::State desiredState = trajectory.Sample(currentTime);
-        auto targetChassisSpeeds = driveController.Calculate(odometry.getPose(), desiredState, trajectory.States().back().pose.Rotation());
-        drive.driveChassisSpeeds(targetChassisSpeeds);
-    }
-
-    void HolonomicTrajectoryCommand::End(bool interrupted) {
-        timer.Stop();
-    }
-
-    bool HolonomicTrajectoryCommand::IsFinished() {
-        return timer.HasElapsed(trajectory.TotalTime());
-    }
+void HolonomicTrajectoryCommand::Initialize() {
+  timer.Reset();
+  timer.Start();
 }
+
+void HolonomicTrajectoryCommand::Execute() {
+  units::time::second_t currentTime = units::second_t(timer.Get());
+  frc::Trajectory::State desiredState = trajectory.Sample(currentTime);
+  auto targetChassisSpeeds =
+      driveController.Calculate(odometry.getPose(), desiredState,
+                                trajectory.States().back().pose.Rotation());
+  drive.driveChassisSpeeds(targetChassisSpeeds);
+}
+
+void HolonomicTrajectoryCommand::End(bool interrupted) { timer.Stop(); }
+
+bool HolonomicTrajectoryCommand::IsFinished() {
+  return timer.HasElapsed(trajectory.TotalTime());
+}
+} // namespace rmb
--- lib/src/drive/MecanumDrive.cpp
+++ lib/src/drive/MecanumDrive.cpp
@@ -2,43 +2,41 @@
 #include "rmb/drive/MecanumDrive.h"
 
 namespace rmb {
-  MecanumDrive::MecanumDrive(VelocityController<units::meters>& fl,
-                             VelocityController<units::meters>& fr,
-                             VelocityController<units::meters>& rl,
-                             VelocityController<units::meters>& rr,
-                             const frc::MecanumDriveKinematics& k,
-                             units::meters_per_second_t maxVel,
-                             units::radians_per_second_t maxRotVel) :
-                             frontLeft(fl), frontRight(fr), rearLeft(rl),
-                             rearRight(rr), kinematics(k),
-                             maxVelocity(maxVel), maxRotVelocity(maxRotVel) {}
+MecanumDrive::MecanumDrive(VelocityController<units::meters> &fl,
+                           VelocityController<units::meters> &fr,
+                           VelocityController<units::meters> &rl,
+                           VelocityController<units::meters> &rr,
+                           const frc::MecanumDriveKinematics &k,
+                           units::meters_per_second_t maxVel,
+                           units::radians_per_second_t maxRotVel)
+    : frontLeft(fl), frontRight(fr), rearLeft(rl), rearRight(rr), kinematics(k),
+      maxVelocity(maxVel), maxRotVelocity(maxRotVel) {}
 
-  void MecanumDrive::driveWheelSpeeds(const frc::MecanumDriveWheelSpeeds& wheelSpeeds) {
-    // Set velocity for each individual wheel
-    frontLeft.setVelocity(wheelSpeeds.frontLeft);
-    frontRight.setVelocity(wheelSpeeds.frontRight);
-    rearLeft.setVelocity(wheelSpeeds.rearLeft);
-    rearRight.setVelocity(wheelSpeeds.rearRight);
-  }
-
-  frc::MecanumDriveWheelSpeeds MecanumDrive::getWheelSpeeds() const {
-    // Return a struct with velocities
-    return {
-      frontLeft.getVelocity(),
-      frontRight.getVelocity(),
-      rearLeft.getVelocity(),
-      rearRight.getVelocity()
-    };
-  }
-
-  void MecanumDrive::driveChassisSpeeds(const frc::ChassisSpeeds& chassisSpeeds, 
-                                        const frc::Translation2d& centerofRotation) {
-    // Get wheel speeds from kinematics
-    frc::MecanumDriveWheelSpeeds wheelSpeeds = kinematics.ToWheelSpeeds(chassisSpeeds, centerofRotation);
-    driveWheelSpeeds(wheelSpeeds);
-  }
-
-  frc::ChassisSpeeds MecanumDrive::getChassisSpeeds() const {
-    return kinematics.ToChassisSpeeds(getWheelSpeeds());
-  }
+void MecanumDrive::driveWheelSpeeds(
+    const frc::MecanumDriveWheelSpeeds &wheelSpeeds) {
+  // Set velocity for each individual wheel
+  frontLeft.setVelocity(wheelSpeeds.frontLeft);
+  frontRight.setVelocity(wheelSpeeds.frontRight);
+  rearLeft.setVelocity(wheelSpeeds.rearLeft);
+  rearRight.setVelocity(wheelSpeeds.rearRight);
 }
+
+frc::MecanumDriveWheelSpeeds MecanumDrive::getWheelSpeeds() const {
+  // Return a struct with velocities
+  return {frontLeft.getVelocity(), frontRight.getVelocity(),
+          rearLeft.getVelocity(), rearRight.getVelocity()};
+}
+
+void MecanumDrive::driveChassisSpeeds(
+    const frc::ChassisSpeeds &chassisSpeeds,
+    const frc::Translation2d &centerofRotation) {
+  // Get wheel speeds from kinematics
+  frc::MecanumDriveWheelSpeeds wheelSpeeds =
+      kinematics.ToWheelSpeeds(chassisSpeeds, centerofRotation);
+  driveWheelSpeeds(wheelSpeeds);
+}
+
+frc::ChassisSpeeds MecanumDrive::getChassisSpeeds() const {
+  return kinematics.ToChassisSpeeds(getWheelSpeeds());
+}
+} // namespace rmb
