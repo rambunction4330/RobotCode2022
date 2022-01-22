9d3f20714d0b99196c7118c2ec4565c477c61a70
Abheek Dhawan <abheekd@protonmail.com>
2022-01-22 12:17:36 -0600

    Auto-format vision source files

--- src/main/cpp/vision/VisionSubsystem.cpp
+++ src/main/cpp/vision/VisionSubsystem.cpp
@@ -7,49 +7,50 @@
 
 void VisionSubsystem::Periodic() {
 
-    static units::radian_t baseRot, shooterAng;
-    this->networkInstance = nt::NetworkTableInstance::GetDefault();
-    if(this->networkInstance.IsConnected())  {
-        baseRot = this->getBaseRotation();
-        shooterAng = this->getShooterAngle();
-        auto table = this->networkInstance.GetTable("ShooterRobot");
-        
-        this->baseRotation = table->GetEntry("baseRotation");
-        this->baseRotation.SetDouble(baseRot.to<double>());
+  static units::radian_t baseRot, shooterAng;
+  this->networkInstance = nt::NetworkTableInstance::GetDefault();
+  if (this->networkInstance.IsConnected()) {
+    baseRot = this->getBaseRotation();
+    shooterAng = this->getShooterAngle();
+    auto table = this->networkInstance.GetTable("ShooterRobot");
 
-        this->shooterAngle = table->GetEntry("shooterAngle");
-        this->shooterAngle.SetDouble(shooterAng.to<double>());
-    }
+    this->baseRotation = table->GetEntry("baseRotation");
+    this->baseRotation.SetDouble(baseRot.to<double>());
+
+    this->shooterAngle = table->GetEntry("shooterAngle");
+    this->shooterAngle.SetDouble(shooterAng.to<double>());
+  }
 }
-
 
 bool VisionSubsystem::IsHubInView() {
-    
-    if(this->networkInstance.IsConnected())  {
-        auto table = this->networkInstance.GetTable("HubData");
-        auto value = table->GetEntry("HubInView");
-        
-        return value.GetBoolean(0);
-    }
 
-    return 0;
+  if (this->networkInstance.IsConnected()) {
+    auto table = this->networkInstance.GetTable("HubData");
+    auto value = table->GetEntry("HubInView");
+
+    return value.GetBoolean(0);
+  }
+
+  return 0;
 }
 
-Vector3<float> VisionSubsystem::GetHubPosition() { //Rotation to hub, horizontal distance to hub, hub height
-    Vector3<float> ret;
-    
-    if(this->networkInstance.IsConnected())  {
-        auto table = this->networkInstance.GetTable("HubData");
-        auto rotationToHub = table->GetEntry("RotationToHub");
-        auto horizontalDistance = table->GetEntry("HorizontalDistanceToHub");
-        auto hubHeight = table->GetEntry("HeightToHub");
+Vector3<float>
+VisionSubsystem::GetHubPosition() { // Rotation to hub, horizontal distance to
+                                    // hub, hub height
+  Vector3<float> ret;
 
-        ret.x = float(rotationToHub.GetDouble(0));
-        ret.y = float(horizontalDistance.GetDouble(0));
-        ret.z = float(hubHeight.GetDouble(0));
+  if (this->networkInstance.IsConnected()) {
+    auto table = this->networkInstance.GetTable("HubData");
+    auto rotationToHub = table->GetEntry("RotationToHub");
+    auto horizontalDistance = table->GetEntry("HorizontalDistanceToHub");
+    auto hubHeight = table->GetEntry("HeightToHub");
 
-        return ret;
-    }
+    ret.x = float(rotationToHub.GetDouble(0));
+    ret.y = float(horizontalDistance.GetDouble(0));
+    ret.z = float(hubHeight.GetDouble(0));
 
-    return { };
+    return ret;
+  }
+
+  return {};
 }
