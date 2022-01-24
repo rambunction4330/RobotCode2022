4ea3b31ac959fc305c17b4f91403cf43a3dd5640
theVerySharpFlat <theVerySharpFlat@users.noreply.github.com>
2022-01-24 02:04:25 +0000

    deploy: 7ee6b0e262a0c28651c6cfbf8aa3b13a266ef1f5

--- classrmb_1_1_spark_max_position_controller.js
+++ classrmb_1_1_spark_max_position_controller.js
@@ -1,4 +1,20 @@
 var classrmb_1_1_spark_max_position_controller =
 [
-    [ "PIDConfig", "structrmb_1_1_spark_max_position_controller_1_1_p_i_d_config.html", null ]
+    [ "PIDConfig", "structrmb_1_1_spark_max_position_controller_1_1_p_i_d_config.html", null ],
+    [ "ConversionUnit", "classrmb_1_1_spark_max_position_controller.html#a2a99f081a78ed274a784a1050ed5c76c", null ],
+    [ "RawAccel", "classrmb_1_1_spark_max_position_controller.html#a8886a87ea550b86e4bd10c15ef2d9576", null ],
+    [ "RawUnit", "classrmb_1_1_spark_max_position_controller.html#a053b922cbb5455a699d8b02efdbcaa24", null ],
+    [ "RawVelocity", "classrmb_1_1_spark_max_position_controller.html#a2c7c1c0cfeed326747887561c3a0b0e7", null ],
+    [ "SparkMaxPositionController", "classrmb_1_1_spark_max_position_controller.html#aec3457d12ead7b95d45480dbe50b5df6", null ],
+    [ "SparkMaxPositionController", "classrmb_1_1_spark_max_position_controller.html#ab51769505640128f628ccc58379db49c", null ],
+    [ "getInverted", "classrmb_1_1_spark_max_position_controller.html#a1962f839d2c11548165c649ab9836d68", null ],
+    [ "getMaxPosition", "classrmb_1_1_spark_max_position_controller.html#ad2d82d32ff37932b80169fea16cdf77d", null ],
+    [ "getMinPosition", "classrmb_1_1_spark_max_position_controller.html#a9b6090522e964e614e9d00003500ba03", null ],
+    [ "getPosition", "classrmb_1_1_spark_max_position_controller.html#ace99f550275a470f03189c538629d80f", null ],
+    [ "getVelocity", "classrmb_1_1_spark_max_position_controller.html#a71010e00f8ad0cc47d1dd62bf41b98b5", null ],
+    [ "resetRefrence", "classrmb_1_1_spark_max_position_controller.html#ac2c596d1e01916b2cc96511d9c604384", null ],
+    [ "setInverted", "classrmb_1_1_spark_max_position_controller.html#a148ef41cf987e23ef20bc84b9862b610", null ],
+    [ "setMaxPosition", "classrmb_1_1_spark_max_position_controller.html#ad9f33c533ade836f17b3bf688e18b4db", null ],
+    [ "setMinPosition", "classrmb_1_1_spark_max_position_controller.html#adbd7c9afc0afef1b6e9d45a8d092c8f7", null ],
+    [ "setPosition", "classrmb_1_1_spark_max_position_controller.html#a1f3fe24d4f01bea217dff1849ca9fb88", null ]
 ];
--- search/all_3.js
+++ search/all_3.js
@@ -1,9 +1,12 @@
 var searchData=
 [
   ['getchassisspeeds_0',['getChassisSpeeds',['../classrmb_1_1_holonomic_drive.html#a82fc55b0ee436c537cda7fff1c7f7e14',1,'rmb::HolonomicDrive::getChassisSpeeds()'],['../classrmb_1_1_mecanum_drive.html#a4450c6fef0e61fdeb7311f1a00ddd703',1,'rmb::MecanumDrive::getChassisSpeeds()']]],
-  ['getinverted_1',['getInverted',['../classrmb_1_1_spark_max_velocity_controller.html#a1ec761427197654dbede306bc90c040a',1,'rmb::SparkMaxVelocityController']]],
-  ['getmaxrotvel_2',['getMaxRotVel',['../classrmb_1_1_holonomic_drive.html#ace175ff6094c4b267af49418233e8879',1,'rmb::HolonomicDrive::getMaxRotVel()'],['../classrmb_1_1_mecanum_drive.html#a30e4d0731a01f20fb2df27237d28b490',1,'rmb::MecanumDrive::getMaxRotVel()']]],
-  ['getmaxvel_3',['getMaxVel',['../classrmb_1_1_holonomic_drive.html#a8c6687fefdfb22d6922b7a5b0708588b',1,'rmb::HolonomicDrive::getMaxVel()'],['../classrmb_1_1_mecanum_drive.html#a4eae098944bb080bba9629f06a583664',1,'rmb::MecanumDrive::getMaxVel()']]],
-  ['getpose_4',['getPose',['../classrmb_1_1_holonomic_drive_odometry.html#afe493ef71434378123a8c9c2b674c980',1,'rmb::HolonomicDriveOdometry::getPose()'],['../classrmb_1_1_mecanum_encoder_odometry.html#a669080c961edd4fff941e4487055ef8f',1,'rmb::MecanumEncoderOdometry::getPose()']]],
-  ['getvelocity_5',['getVelocity',['../classrmb_1_1_spark_max_velocity_controller.html#a8184fafcf6b0d12865bc87cc2663f708',1,'rmb::SparkMaxVelocityController']]]
+  ['getinverted_1',['getInverted',['../classrmb_1_1_spark_max_position_controller.html#a1962f839d2c11548165c649ab9836d68',1,'rmb::SparkMaxPositionController::getInverted()'],['../classrmb_1_1_spark_max_velocity_controller.html#a1ec761427197654dbede306bc90c040a',1,'rmb::SparkMaxVelocityController::getInverted()']]],
+  ['getmaxposition_2',['getMaxPosition',['../classrmb_1_1_spark_max_position_controller.html#ad2d82d32ff37932b80169fea16cdf77d',1,'rmb::SparkMaxPositionController']]],
+  ['getmaxrotvel_3',['getMaxRotVel',['../classrmb_1_1_holonomic_drive.html#ace175ff6094c4b267af49418233e8879',1,'rmb::HolonomicDrive::getMaxRotVel()'],['../classrmb_1_1_mecanum_drive.html#a30e4d0731a01f20fb2df27237d28b490',1,'rmb::MecanumDrive::getMaxRotVel()']]],
+  ['getmaxvel_4',['getMaxVel',['../classrmb_1_1_holonomic_drive.html#a8c6687fefdfb22d6922b7a5b0708588b',1,'rmb::HolonomicDrive::getMaxVel()'],['../classrmb_1_1_mecanum_drive.html#a4eae098944bb080bba9629f06a583664',1,'rmb::MecanumDrive::getMaxVel()']]],
+  ['getminposition_5',['getMinPosition',['../classrmb_1_1_spark_max_position_controller.html#a9b6090522e964e614e9d00003500ba03',1,'rmb::SparkMaxPositionController']]],
+  ['getpose_6',['getPose',['../classrmb_1_1_holonomic_drive_odometry.html#afe493ef71434378123a8c9c2b674c980',1,'rmb::HolonomicDriveOdometry::getPose()'],['../classrmb_1_1_mecanum_encoder_odometry.html#a669080c961edd4fff941e4487055ef8f',1,'rmb::MecanumEncoderOdometry::getPose()']]],
+  ['getposition_7',['getPosition',['../classrmb_1_1_spark_max_position_controller.html#ace99f550275a470f03189c538629d80f',1,'rmb::SparkMaxPositionController']]],
+  ['getvelocity_8',['getVelocity',['../classrmb_1_1_spark_max_position_controller.html#a71010e00f8ad0cc47d1dd62bf41b98b5',1,'rmb::SparkMaxPositionController::getVelocity()'],['../classrmb_1_1_spark_max_velocity_controller.html#a8184fafcf6b0d12865bc87cc2663f708',1,'rmb::SparkMaxVelocityController::getVelocity()']]]
 ];
