1b628936407cf45648ab5f1aa3e8c9629cb8c104
eschreiner1717 <98493270+eschreiner1717@users.noreply.github.com>
2022-01-26 16:47:11 -0600

    Add initial functions for intake subsystem

--- src/main/include/intake/IntakeSubsystem.h
+++ src/main/include/intake/IntakeSubsystem.h
@@ -5,6 +5,11 @@
 #pragma once
 
 #include <frc2/command/SubsystemBase.h>
+#include <rmb/motorcontrol/SparkMax/SparkMaxVelocityController.h>
+#include <rmb/motorcontrol/SparkMax/SparkMaxPositionController.h>
+#include <units/length.h>
+#include <units/velocity.h>
+
 
 class IntakeSubsystem : public frc2::SubsystemBase {
  public:
@@ -18,4 +23,27 @@ class IntakeSubsystem : public frc2::SubsystemBase {
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+  rmb::SparkMaxVelocityController<units::meters> intakeMotorVel;
+  rmb::SparkMaxPositionController<units::meters> intakeMotorPos;
+
+  void deployIntake() { //moves intake out from starting pos 
+    intakeMotorPos.setPosition(100_m);
+  }
+
+  void disengageIntake() { //moves intake to starting pos
+    intakeMotorPos.setPosition(0_m);
+  }
+
+  void stopSpinning() { //stops spinning discs to collect balls
+    intakeMotorVel.setVelocity(0_mps);
+  }
+
+  void startSpinning() { //starts spinning discs to collect balls
+    intakeMotorVel.setVelocity(100_mps);
+  }
+
+
+
+
+
 };
