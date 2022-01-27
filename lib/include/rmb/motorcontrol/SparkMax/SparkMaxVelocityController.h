9c91d859c9f10f7ca7c66cb6e9a42be58a8d15b1
gcjurgiel <gcjurgiel@icloud.com>
2022-01-26 20:56:36 -0600

    formatting

--- lib/include/rmb/motorcontrol/SparkMax/SparkMaxVelocityController.h
+++ lib/include/rmb/motorcontrol/SparkMax/SparkMaxVelocityController.h
@@ -5,8 +5,8 @@
 
 #include "rmb/motorcontrol/VelocityController.h"
 #include "rmb/motorcontrol/feedforward/ArmFeedforward.h"
-#include "rmb/motorcontrol/feedforward/SimpleMotorFeedforward.h"
 #include "rmb/motorcontrol/feedforward/ElevatorFeedforward.h"
+#include "rmb/motorcontrol/feedforward/SimpleMotorFeedforward.h"
 
 #include <rev/CANSparkMax.h>
 
@@ -59,7 +59,11 @@ public:
         rev::SparkMaxPIDController::AccelStrategy::kSCurve;
   };
 
-  struct Follower { int id; rev::CANSparkMax::MotorType motorType; bool inverted; };
+  struct Follower {
+    int id;
+    rev::CANSparkMax::MotorType motorType;
+    bool inverted;
+  };
 
   SparkMaxVelocityController(int deviceID);
   SparkMaxVelocityController(
--- lib/src/motorcontrol/SparkMax/SparkMaxVelocityController.cpp
+++ lib/src/motorcontrol/SparkMax/SparkMaxVelocityController.cpp
@@ -52,10 +52,10 @@ SparkMaxVelocityController<U>::SparkMaxVelocityController(
     controlType = rev::CANSparkMax::ControlType::kVelocity;
   }
 
-
   followers.reserve(followerList.size());
-  for (const auto& follower: followerList) {
-    followers.emplace_back(new rev::CANSparkMax(follower.id, follower.motorType));
+  for (const auto &follower : followerList) {
+    followers.emplace_back(
+        new rev::CANSparkMax(follower.id, follower.motorType));
     followers.back()->Follow(sparkMax, follower.inverted);
   }
 }
