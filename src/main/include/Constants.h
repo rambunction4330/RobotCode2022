a562d3c6b18b2745ec80fa12b2b7db9abfb1f92b
gcjurgiel <gcjurgiel@icloud.com>
2022-01-27 18:52:05 -0600

    drive subsystem constants

--- src/main/include/Constants.h
+++ src/main/include/Constants.h
@@ -4,12 +4,52 @@
 
 #pragma once
 
-/**
- * The Constants header provides a convenient place for teams to hold robot-wide
- * numerical or boolean constants.  This should not be used for any other
- * purpose.
- *
- * It is generally a good idea to place constants into subsystem- or
- * command-specific namespaces within this header, which can then be used where
- * they are needed.
- */
+#include <units/length.h>
+#include <units/velocity.h>
+#include <units/acceleration.h>
+
+#include <units/angle.h>
+#include <units/angular_velocity.h>
+#include <units/angular_acceleration.h>
+
+#include <frc/geometry/Translation2d.h>
+
+#include <AHRS.h>
+
+#include <rmb/motorcontrol/feedforward/SimpleMotorFeedforward.h>
+#include <rmb/motorcontrol/SparkMax/SparkMaxVelocityController.h>
+
+namespace driveSubsystemConstants {
+  const static int frontLeftID = 1, frontRightID = 2, 
+                   rearLeftID  = 3, rearRightID  = 4;
+
+  const static frc::Translation2d frontLeftPose(-0.381_m, 0.381_m),
+                                  frontRightPose(0.381_m, 0.381_m), 
+                                  rearLeftPose(-0.381_m, -0.381_m), 
+                                  rearRightPose(0.381_m, -0.381_m);
+  
+  const static rmb::SparkMaxVelocityController<units::meters>::PIDConfig motorPIDConfig{
+    /* p */ 0.0057181, /* i */ 0.0, /* d */ 0.0, /* f */ 0.0,
+    /* iZone */ 0.0, /* iMaxAccumulator */ 0.0,
+    /* maxOutput */ 1.0, /* minOutput */ -1.0,
+
+    /* SmartMotion config */
+    /* usingSmartMotion */ true,
+    /* maxVelocity */ 25_mps, /* minVelocity */ 0_mps,
+    /* maxAccel */ 10_mps_sq,
+    /* allowedErr */ 0.9_mps,
+    /* accelStrategy */ rev::SparkMaxPIDController::AccelStrategy::kSCurve
+  };
+
+  const static rmb::SimpleMotorFeedforward<units::meters> motorFeedforward(
+    rmb::SimpleMotorFeedforward<units::meters>::Ks_t(0.0), 
+    rmb::SimpleMotorFeedforward<units::meters>::Kv_t(0.0), 
+    rmb::SimpleMotorFeedforward<units::meters>::Ka_t(0.0)
+  );
+
+  const static rmb::SparkMaxVelocityController<units::meters>::ConversionUnit_t motorConvertion(0.04);
+
+  const static units::meters_per_second_t maxXVelocity(0.0);
+  const static units::meters_per_second_t maxYVelocity(0.0);
+  const static units::radians_per_second_t maxRotVelocity(0.0);
+}
