8e4247f1e60541054755026bc55d0f63ad2974b2
gcjurgiel <gcjurgiel@icloud.com>
2022-01-27 18:57:37 -0600

    removed useless comment

--- src/main/include/drivetrain/DriveSubsystem.h
+++ src/main/include/drivetrain/DriveSubsystem.h
@@ -32,6 +32,4 @@ private:
   rmb::MecanumDrive drive;
   AHRS gyro;
   rmb::MecanumEncoderOdometry odometry;
-  // Components (e.g. motor controllers and sensors) should generally be
-  // declared private and exposed only through public methods.
 };
