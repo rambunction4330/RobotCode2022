7ef48c33b488ce5fc5921cd99e034ac71b3a0735
Abheek Dhawan <abheekd@protonmail.com>
2022-02-11 21:43:19 -0600

    Add documentation for the mecanum encoder odometry

--- lib/include/rmb/drive/MecanumEncoderOdometry.h
+++ lib/include/rmb/drive/MecanumEncoderOdometry.h
@@ -9,11 +9,37 @@
 namespace rmb {
 class MecanumEncoderOdometry : public DriveOdometry {
 public:
+  /** 
+   * Creates a MecanumEncoderOdemetry with the mecanum drive,
+   * gyro, and initial pose (location)
+   * @param drive Mecanum drive of the robot
+   * @param gyro The gyroscope
+   * @param initialPose The initial position of the robot
+   */
   MecanumEncoderOdometry(MecanumDrive &drive, const frc::Gyro &gyro,
                          const frc::Pose2d &initalPose = frc::Pose2d());
 
+  /**
+   * Getter to return the current pose
+   * @return The current position
+   * @see updatePose()
+   * @see resetPose()
+   */
   const frc::Pose2d &getPose() const;
+
+  /**
+   * Function to update the pose
+   * @return The update position
+   * @see getPose()
+   * @see resetPose()
+   */
   const frc::Pose2d &updatePose();
+
+  /**
+   * Function to reset the pose
+   * @see getPose()
+   * @see updatePose()
+   */
   void resetPose(const frc::Pose2d &pose);
 
 private:
