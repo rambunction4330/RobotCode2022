70e9ea2ca70d5ff52d9e74dab5c9ab39e7bb1ff3
theVerySharpFlat <theVerySharpFlat@gmail.com>
2022-02-19 21:13:59 -0600

    initial commit

---
+++ lib/include/rmb/math/trajectory/trajectory.h
@@ -0,0 +1,21 @@
+/** \file trajectory.h
+ * This file contains useful trajectory functions to calculate the angle and speed a ball needs to
+ * exit a shooter in order to land in the correct position
+ */
+
+#pragma once
+
+namespace rmb {
+
+namespace trajectory {
+
+    /**
+     * This is a function that should get documented
+     */
+    int aFunction() {
+        
+    }
+
+}
+
+}
