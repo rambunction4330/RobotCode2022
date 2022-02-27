65741fa84315e44c4dce67fbf9a287040e0f3da3
gcjurgiel <gcjurgiel@users.noreply.github.com>
2022-02-27 21:38:23 +0000

    deploy: 48b9260cd4aa848d6ad9700cbb114f66551f7883

--- classrmb_1_1_spark_max_velocity_controller.js
+++ classrmb_1_1_spark_max_velocity_controller.js
@@ -7,7 +7,7 @@ var classrmb_1_1_spark_max_velocity_controller =
     [ "RawUnit", "classrmb_1_1_spark_max_velocity_controller.html#a0421c166263788f0d6b996a1c8202684", null ],
     [ "RawVelocity", "classrmb_1_1_spark_max_velocity_controller.html#a27a85b4840275a072e9ce22f31e44b1f", null ],
     [ "SparkMaxVelocityController", "classrmb_1_1_spark_max_velocity_controller.html#a774260dfcb78054b40c1723550a65d61", null ],
-    [ "SparkMaxVelocityController", "classrmb_1_1_spark_max_velocity_controller.html#ad43ca3f4300a5e96846d104e74799bc5", null ],
+    [ "SparkMaxVelocityController", "classrmb_1_1_spark_max_velocity_controller.html#acc3284cb6f11ac74bd42669964c864ec", null ],
     [ "getInverted", "classrmb_1_1_spark_max_velocity_controller.html#a1ec761427197654dbede306bc90c040a", null ],
     [ "getVelocity", "classrmb_1_1_spark_max_velocity_controller.html#a8184fafcf6b0d12865bc87cc2663f708", null ],
     [ "setInverted", "classrmb_1_1_spark_max_velocity_controller.html#a92342213fd1c8d33064b64c6c24da82f", null ],
